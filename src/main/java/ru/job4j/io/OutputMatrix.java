6f6ee94791776d09ea05ffd83293fb3161af84ea
PavelM2205 <pavelm2205@mail.ru>
2022-04-06 17:03:05 +0700

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/io/OutputMatrix.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class OutputMatrix {
+
+    public static int[][] multiple(int size) {
+        int[][] result = new int[size][size];
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                result[row][cell] = (row + 1) * (cell + 1);
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[][] mas = multiple(5);
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 0; i < mas.length; i++) {
+                for (int j = 0; j < mas.length; j++) {
+                    out.write(String.valueOf(mas[i][j]).getBytes());
+                    out.write(" ".getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+}
+
