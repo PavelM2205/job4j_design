7d5ef3bce6da4c7ba0a361e63d33230fdd615216
PavelM2205 <pavelm2205@mail.ru>
2022-04-19 12:57:18 +0700

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -8,17 +8,24 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
+
+    private static boolean closeable = false;
+
     public static void main(String[] args) throws IOException {
-        boolean closeable;
+        String answer;
+        String firstLine;
         try (ServerSocket server = new ServerSocket(9000)) {
             while (!server.isClosed()) {
                 Socket socket = server.accept();
                 try (OutputStream out = socket.getOutputStream();
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
+                    firstLine = in.readLine();
+                    answer = getAnswer(firstLine);
                     out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String firstLine = in.readLine();
-                    closeable = firstLine.matches(".+msg=Bye.+");
+                    if (!closeable) {
+                        out.write(answer.getBytes());
+                    }
                     System.out.println(firstLine);
                     for (String str = in.readLine(); str != null && !str.isEmpty();
                          str = in.readLine()) {
@@ -31,5 +38,20 @@ public class EchoServer {
                 }
             }
         }
+    }
+
+    private static String getAnswer(String string) {
+        String result;
+        if (string.matches(".+msg=Hello.+")) {
+            result = "Hello, dear friend!";
+        } else if (string.matches(".+msg=Exit.+")) {
+            closeable = true;
+            result = "";
+        } else {
+            int start = string.indexOf("=") + 1;
+            int end = string.lastIndexOf(" ");
+            result = string.substring(start, end);
+        }
+        return result;
     }
 }
