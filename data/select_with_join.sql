0094abada39a61232b70f2e3abb41c6504cc59b9
PavelM2205 <pavelm2205@mail.ru>
2022-05-12 17:08:59 +0700

    1. Inner join [#373617]

--- data/select_with_join.sql
+++ data/select_with_join.sql
@@ -1,3 +1,39 @@
+create table town(
+	id serial primary key,
+	name varchar(255)
+);
+create table post_index(
+	id serial primary key,
+	index decimal(6)
+);
+create table street(
+	id serial primary key,
+	name varchar(255)
+);
+create table citizen(
+	id serial primary key,
+	name varchar(255),
+	town_id int references town(id),
+	street_id int references street(id),
+	post_index_id int references post_index(id)
+);
+
+insert into town(name) values('Москва');
+insert into town(name) values('Красноярск');
+insert into town(name) values('Владивосток');
+
+insert into street(name) values('Новая басманная');
+insert into street(name) values('Ленина');
+insert into street(name) values('Луговая');
+
+insert into post_index(index) values(107174);
+insert into post_index(index) values(660045);
+insert into post_index(index) values(690037);
+
+insert into citizen(name, town_id, street_id, post_index_id) values('Ivan', 1, 1, 1);
+insert into citizen(name, town_id, street_id, post_index_id) values('Maxim', 2, 2, 2);
+insert into citizen(name, town_id, street_id, post_index_id) values('Alexey', 3, 3, 3);
+
 select c.name, t.name from citizen as c
 inner join town as t
 on c.town_id = t.id;
